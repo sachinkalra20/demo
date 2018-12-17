4d510d071459ca9685f2edd9dfc79c0f2a4ce315
sachin <skjcdmcoe@gmail.com>
2018-12-17 16:48:31 +0530

    commit1

---
+++ demo2.py
@@ -0,0 +1,12 @@
+x=int(input("enter any number"))
+if x>=0:
+    if x==0:
+        print("zero")
+    else:
+        print("positive number")
+else:
+    print("negative number")
+    
+            
+            
+    
---
+++ demo5.py
@@ -0,0 +1,6 @@
+x=int(input("enter any number"))
+print(x)
+if not x==50:
+  print("the value is different from 50")
+else:
+  print("the value of x equal to 50")
